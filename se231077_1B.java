21e9058cc9db588c0a4f4eb4d5704e64a1bf4a94
Aun <aunalirehmani@gmail.com>
2024-11-20 22:25:40 +0500

    new commit

--- se231077_1B.java
+++ se231077_1B.java
@@ -1,20 +1,36 @@
+import java.util.Arrays;
+
 public class se231077_1B {
 
     String name[] = {"adam", "clark", "evens", "salim", "jones", "lane", "pace", "smith"};
-    String insert="davis";
-    int atindex=0;
-    int sizeofarray= name.length;
+    String insert = "davis";
+    int atindex = 2;
+    int size = 9;
+    String[] nam;
 
-    void insert(){
-        if(sizeofarray>=atindex){
-            name[atindex]=insert;
+    void insert() {
+        if (size > atindex) {
+            nam = new String[size];
 
-        }
-        else {
-            System.out.println("array is big cant insert");
-        }
-        for (int i=0 ; i<sizeofarray; i++){
-            System.out.print(name[i]+",");
+
+            for (int i = 0; i < atindex; i++) {
+                nam[i] = name[i];
+            }
+
+
+            nam[atindex] = insert;
+
+
+            for (int i = atindex + 1; i < size; i++) {
+                nam[i] = name[i - 1];
+            }
+
+
+            for (int i = 0; i < size; i++) {
+                System.out.print(nam[i] + ",");
+            }
+        } else {
+            System.out.println("Array is too small to insert.");
         }
     }
 
@@ -22,7 +38,4 @@ public class se231077_1B {
         se231077_1B obj = new se231077_1B();
         obj.insert();
     }
-
 }
-
-
--- se231077_3B.java
+++ se231077_3B.java
@@ -9,14 +9,15 @@ public class se231077_3B
 
     int base=300;
     int lower_bound=5;
-
+    int word_size=3;
     void formula(){
         Scanner scan= new Scanner(System.in);
         System.out.println("enter index value < 50:");
         int i= scan.nextInt();
 
-        int address=(base+(i-lower_bound));
+        int address=(base+(i-lower_bound)*word_size);
         System.out.println("the address of AAA" +i+" array at specific element is:"+address);
+        System.out.println("the word size is supposed as 3");
     }
 
     public static void main(String[] args) {
